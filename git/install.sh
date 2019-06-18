105ff8d277b7cfa2b48b9a714d0ff3754880f7d9
Jakub Chledowski <jakub.chledowski@gmail.com>
2019-06-18 14:45:34 +0200

    Update install.sh

--- git/install.sh
+++ git/install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -e
 
-ln -s $HOME/.dots/git/gitconfig $HOME/.gitconfig
+ln -s /data/user/git/gitconfig $HOME/.gitconfig
 mkdir -p $HOME/.ssh
 ln -s /data/user/.ssh/id_rsa $HOME/.ssh/id_rsa
 ln -s /data/user/.ssh/id_rsa.pub $HOME/.ssh/id_rsa.pub
