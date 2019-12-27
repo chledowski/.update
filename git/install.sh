7bfbef1ea6359c16b319022f756badf5a75c6e9c
Jakub Chledowski <jakub.chledowski@gmail.com>
2019-12-27 10:22:48 +0100

    Update install.sh

--- git/install.sh
+++ git/install.sh
@@ -4,3 +4,7 @@ ln -s /data/user/git/gitconfig $HOME/.gitconfig
 mkdir -p $HOME/.ssh
 ln -s /data/user/.ssh/id_rsa $HOME/.ssh/id_rsa
 ln -s /data/user/.ssh/id_rsa.pub $HOME/.ssh/id_rsa.pub
+
+# LFS
+curl -s https://packagecloud.io/install/repositories/github/git-lfs/script.deb.sh | bash
+apt-get install git-lfs
